ad2d6c27fd860af83787beddd6fcc02ccc14005c
harish-7052 <harish-7052@csez.zohocorpin.com>
2020-07-30 08:54:59 +0530

    Updates

---
+++ precache-manifest.a4d8a32cb3cdb32eb6b779d115fe9fff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a1ffabc7cbfb825081b6bce4eec1a8",
+    "url": "/Learn/index.html"
+  },
+  {
+    "revision": "8c6ce2a921fe3a62b08d",
+    "url": "/Learn/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d261a86de89d7c666b3a",
+    "url": "/Learn/static/js/2.a890cf5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Learn/static/js/2.a890cf5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c6ce2a921fe3a62b08d",
+    "url": "/Learn/static/js/main.89b8b3ae.chunk.js"
+  },
+  {
+    "revision": "162641f57dbf86090a21",
+    "url": "/Learn/static/js/runtime-main.cd1b23d4.js"
+  }
+]);
